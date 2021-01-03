6ee525708a35b735b1fac3a4eca5ff7a0838627f
Tomas Reimers <tomasreimers@gmail.com>
2021-01-02 21:39:55 -0500

    Automated Release

--- dist/lib/api.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="node" />
-import * as t from "retype";
-import { TRoute } from "shared-routes";
-export declare function endpointWithArgs<TActualRoute extends TRoute>(route: TActualRoute & {
-    method: "GET";
-}, queryParams?: t.UnwrapSchemaMap<TActualRoute["queryParams"]>, urlParams?: t.UnwrapSchemaMap<TActualRoute["urlParams"]>): string;
-export declare function requestWithArgs<TActualRoute extends TRoute>(route: TActualRoute, params: TActualRoute["rawBody"] extends true ? Buffer : t.UnwrapSchemaMap<TActualRoute["params"]>, queryParams?: t.UnwrapSchemaMap<TActualRoute["queryParams"]>, urlParams?: t.UnwrapSchemaMap<TActualRoute["urlParams"]>): Promise<t.UnwrapSchemaMap<TActualRoute["response"]>>;
